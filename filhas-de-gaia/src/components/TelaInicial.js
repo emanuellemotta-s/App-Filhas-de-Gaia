b71849228c8043035151b4087aa1c32114987841
Emanuelle <emanuelle.srmotta@gmail.com>
2024-01-20 15:27:53 -0300

    Removi arquivo firebaseConnection e adicionei componentes na Tela Inicial

--- filhas-de-gaia/src/components/TelaInicial.js
+++ filhas-de-gaia/src/components/TelaInicial.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image} from 'react-native';
 import Button from './Button';
 import Logo from './Logo';
+import Titulo from './Titulo';
 
 export default function TelaInicial(){
     return(
         <View style={styles.container}>
+            <View style={styles.barraSuperior}>
+                <Image source={require("../assets/perfil vazio.png")}/>
+                <Titulo text="Olá, Nome do usuário"/>
+            </View>
+
             <StatusBar hidden/>
             <Logo/>
             <Button text="NOVO HÁBITO"/>
+            
+            <View></View>
         </View>
     );
 }
@@ -21,5 +29,10 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent:'center',
         padding:20
-      }
+      },
+    
+    barraSuperior: {
+        flex: 'row',
+        backgroundColor: '#8c8cc2'
+    }
 });
--- filhas-de-gaia/src/components/Titulo.js
+++ filhas-de-gaia/src/components/Titulo.js
@@ -5,7 +5,7 @@ import { StyleSheet } from "react-native";
 export default function Titulo(props){
     return(
         <Text style={styles.text}>{props.text}</Text>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
@@ -15,4 +15,4 @@ const styles = StyleSheet.create({
         margin: 10,
         padding: 15
     }
-})
+});
